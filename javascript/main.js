e6416a9c0534480d9929e2a2daf3b6c2600b8b04
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2022-08-09 19:52:15 +0600

    Test
    Test

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+$('.teams__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+}); 
